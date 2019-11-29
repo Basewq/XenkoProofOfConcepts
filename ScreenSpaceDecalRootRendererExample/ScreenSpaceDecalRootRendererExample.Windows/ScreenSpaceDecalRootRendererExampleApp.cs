791b3842d07d10025e7203ffdf59e58f08d9bb0a
Basewq <Basewq@users.noreply.github.com>
2019-11-30 10:46:11 +1300

    [SSDR] Initial project commit with 'New game' template for ScreenSpaceDecalRootRendererExample.

---
+++ ScreenSpaceDecalRootRendererExample/ScreenSpaceDecalRootRendererExample.Windows/ScreenSpaceDecalRootRendererExampleApp.cs
@@ -0,0 +1,15 @@
+using Xenko.Engine;
+
+namespace ScreenSpaceDecalRootRendererExample.Windows
+{
+    class ScreenSpaceDecalRootRendererExampleApp
+    {
+        static void Main(string[] args)
+        {
+            using (var game = new Game())
+            {
+                game.Run();
+            }
+        }
+    }
+}
