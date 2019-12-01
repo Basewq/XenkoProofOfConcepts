d28c67f4ada2f23a74082d9c132aa3a096479a6c
Basewq <Basewq@users.noreply.github.com>
2019-12-02 00:24:50 +1300

    [OIRT] Initial project commit with 'New game' template and Materials pack for ObjectInfoRenderTargetExample.

---
+++ ObjectInfoRenderTargetExample/ObjectInfoRenderTargetExample.Windows/ObjectInfoRenderTargetExampleApp.cs
@@ -0,0 +1,15 @@
+using Xenko.Engine;
+
+namespace ObjectInfoRenderTargetExample.Windows
+{
+    class ObjectInfoRenderTargetExampleApp
+    {
+        static void Main(string[] args)
+        {
+            using (var game = new Game())
+            {
+                game.Run();
+            }
+        }
+    }
+}
