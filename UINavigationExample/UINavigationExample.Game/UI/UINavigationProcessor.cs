bb252a0aaf314ee039d869ec16514733ec9061e4
Basewq <Basewq@users.noreply.github.com>
2022-09-20 11:48:22 +1200

    [UNE] UI Navigation remove unnecessary lists, code cleanup.

--- UINavigationExample/UINavigationExample.Game/UI/UINavigationProcessor.cs
+++ UINavigationExample/UINavigationExample.Game/UI/UINavigationProcessor.cs
@@ -18,10 +18,9 @@ namespace UINavigationExample.UI
         private readonly List<UIComponent> _uiComponents = new List<UIComponent>();
         private InputManager _inputManager;
 
-        private UINavControl _selectedControl = default;
+        private UINavControl? _selectedControl = null;
 
         private readonly List<UINavControl> _allActiveControls = new List<UINavControl>();
-        private readonly List<UINavControl> _allActiveControlsExclSelected = new List<UINavControl>();
         private bool _rebuildActiveControlsList = true;
 
         protected override void OnSystemAdd()
@@ -38,20 +37,20 @@ namespace UINavigationExample.UI
         protected override void OnEntityComponentRemoved(Entity entity, [NotNull] UIComponent component, [NotNull] UIComponent data)
         {
             _uiComponents.Remove(component);
-            if (_selectedControl.UIComponent == component)
+            if (_selectedControl?.UIComponent == component)
             {
                 UnsetSelectedControl();
             }
             _rebuildActiveControlsList = true;
         }
 
-        private readonly List<UINavControl> _nextControlCandidates = new List<UINavControl>();
         public override void Update(GameTime time)
         {
+            var selectedNavCtrl = _selectedControl?.Control;
+
             if (_rebuildActiveControlsList)
             {
                 _allActiveControls.Clear();
-                _allActiveControlsExclSelected.Clear();
                 // Find all active navigatable controls
                 foreach (var uiComp in _uiComponents)
                 {
@@ -61,29 +60,24 @@ namespace UINavigationExample.UI
                     }
                     var rootElement = uiComp.Page.RootElement;
                     var navControls = FindAllNavigatableControls(rootElement);
-                    var activeNavControls = navControls.Where(x => x.IsSelectable && x.CanNavigateToControl() && x != _selectedControl.Control);
+                    var activeNavControls = navControls.Where(x => x.IsSelectable && x.CanNavigateToControl() && x != selectedNavCtrl);
 
                     _allActiveControls.AddRange(activeNavControls.Select(x => new UINavControl(uiComp, x)));
-                    _allActiveControlsExclSelected.AddRange(_allActiveControls);
-                    if (_selectedControl.Control != null)
-                    {
-                        int idx = _allActiveControlsExclSelected.IndexOf(x => x.Control == _selectedControl.Control);
-                        if (idx >= 0)
-                        {
-                            _allActiveControlsExclSelected.RemoveAt(idx);
-                        }
-                    }
                 }
                 _rebuildActiveControlsList = false;
             }
 
             bool selectionRequestedHandled = false;
-            if (_selectedControl.Control?.HasFocusExternally ?? false)
+            if (selectedNavCtrl?.HasFocusExternally ?? false)
             {
-                _selectedControl.Control.HasFocusExternally = false;      // Already selected
+                selectedNavCtrl.HasFocusExternally = false;      // Already selected
             }
-            foreach (var activeCtrl in _allActiveControlsExclSelected)
+            foreach (var activeCtrl in _allActiveControls)
             {
+                if (activeCtrl.Control == selectedNavCtrl)
+                {
+                    continue;
+                }
                 if (activeCtrl.Control.HasFocusExternally)
                 {
                     UnsetSelectedControl();
@@ -91,13 +85,13 @@ namespace UINavigationExample.UI
                     activeCtrl.Control.HasFocusExternally = false;
                     selectionRequestedHandled = true;
                 }
-                // Don't break, because if multiple external focuses occur, we need to accept them all
+                // Don't break for-loop, because if multiple external focuses occur, we need to accept them all
                 // (which will immediately unset the previous one, since we can't reject the previous)
             }
 
             if (!selectionRequestedHandled)
             {
-                HandleInputMovement(_allActiveControls, _allActiveControlsExclSelected);
+                HandleInputMovement(_allActiveControls);
                 bool isCommittingSelection = _inputManager.IsKeyPressed(Keys.Enter) || _inputManager.IsKeyPressed(Keys.Space);
                 if (_inputManager.HasGamePad && _inputManager.DefaultGamePad != null)
                 {
@@ -106,15 +100,17 @@ namespace UINavigationExample.UI
                         || gamePad.IsButtonPressed(GamePadButton.A)
                         || gamePad.IsButtonPressed(GamePadButton.X);
                 }
-                if (isCommittingSelection && _selectedControl.Control != null)
+                if (isCommittingSelection && selectedNavCtrl != null)
                 {
-                    _selectedControl.Control.OnNavigationCommitSelection();
+                    selectedNavCtrl.OnNavigationCommitSelection();
                 }
             }
         }
 
-        private void HandleInputMovement(List<UINavControl> allActiveControls, List<UINavControl> allActiveControlsExclSelected)
+        private void HandleInputMovement(List<UINavControl> allActiveControls)
         {
+            var selectedNavCtrl = _selectedControl?.Control;
+
             bool isTabPressed = _inputManager.IsKeyPressed(Keys.Tab);
             bool isShiftKeyDown = _inputManager.IsKeyDown(Keys.LeftShift) || _inputManager.IsKeyDown(Keys.RightShift);
             if (_inputManager.HasGamePad && _inputManager.DefaultGamePad != null)
@@ -132,14 +128,14 @@ namespace UINavigationExample.UI
                     isTabPressed = true;
                 }
             }
-            if (_selectedControl.Control != null && isTabPressed)
+            if (selectedNavCtrl != null && isTabPressed)
             {
                 var navMmnt = new UINavigationInputMovement
                 {
                     InputType = isShiftKeyDown ? UINavigationInputType.ShiftTab : UINavigationInputType.Tab,
                     InputDirection = Vector2.Zero
                 };
-                if (_selectedControl.Control.OnNavigationMovement(navMmnt))
+                if (selectedNavCtrl.OnNavigationMovement(navMmnt))
                 {
                     // Current control has handled the input
                     return;
@@ -166,14 +162,14 @@ namespace UINavigationExample.UI
             {
                 inputDir.Normalize();
             }
-            if (_selectedControl.Control != null && inputDir.LengthSquared() > 0)
+            if (selectedNavCtrl != null && inputDir.LengthSquared() > 0)
             {
                 var navMmnt = new UINavigationInputMovement
                 {
                     InputType = UINavigationInputType.Input,
                     InputDirection = inputDir
                 };
-                if (_selectedControl.Control.OnNavigationMovement(navMmnt))
+                if (selectedNavCtrl.OnNavigationMovement(navMmnt))
                 {
                     // Current control has handled the input
                     return;
@@ -193,74 +189,28 @@ namespace UINavigationExample.UI
 
             if (inputDir.LengthSquared() > 0)
             {
-                if (_selectedControl.UIElement == null)
+                if (!_selectedControl.HasValue)
                 {
                     // Just pick the first control
                     if (allActiveControls.Count > 0)
                     {
                         // Sort top to bottom, left to right
-                        allActiveControls.Sort((ctrl1, ctrl2) =>
-                        {
-                            // Use the top-left corner of the UI as the sorting position, as this should
-                            // account for different sized UI
-
-                            var ctrl1Pos = ctrl1.UIElement.WorldMatrix.TranslationVector;
-                            float ctrl1Left = ctrl1Pos.X - (ctrl1.UIElement.RenderSize.X * 0.5f);
-                            float ctrl1Top = ctrl1Pos.Y - (ctrl1.UIElement.RenderSize.Y * 0.5f);
-
-                            var ctrl2Pos = ctrl2.UIElement.WorldMatrix.TranslationVector;
-                            float ctrl2Left = ctrl2Pos.X - (ctrl2.UIElement.RenderSize.X * 0.5f);
-                            float ctrl2Top = ctrl2Pos.Y - (ctrl2.UIElement.RenderSize.Y * 0.5f);
-                            if (ctrl1Top < ctrl2Top)
-                            {
-                                return -1;
-                            }
-                            else if (ctrl1Top > ctrl2Top)
-                            {
-                                return 1;
-                            }
-                            else
-                            {
-                                return ctrl1Left.CompareTo(ctrl2Left);
-                            }
-                        });
+                        allActiveControls.Sort(TopDownThenLeftRightSorter);
                         SetControlAsSelected(allActiveControls[0]);
                     }
                     //else  // Nothing to select
                 }
-                else if (allActiveControlsExclSelected.Count > 0)
+                else if (_selectedControl.HasValue && allActiveControls.Count > 1)
                 {
-                    var uiWorldMatrix = _selectedControl.UIElement.WorldMatrix;
+                    // Navigate to next control
+                    var selectedCtrl = _selectedControl.Value;
+                    var uiWorldMatrix = selectedCtrl.UIElement.WorldMatrix;
                     var originUIPosition = uiWorldMatrix.TranslationVector.XY();
                     if (Math.Abs(inputDir.X) >= Math.Abs(inputDir.Y))
                     {
                         // Sort top to bottom, left to right
-                        allActiveControls.Sort((ctrl1, ctrl2) =>
-                        {
-                            // Use the top-left corner of the UI as the sorting position, as this should
-                            // account for different sized UI
-
-                            var ctrl1Pos = ctrl1.UIElement.WorldMatrix.TranslationVector;
-                            float ctrl1Left = ctrl1Pos.X - (ctrl1.UIElement.RenderSize.X * 0.5f);
-                            float ctrl1Top = ctrl1Pos.Y - (ctrl1.UIElement.RenderSize.Y * 0.5f);
-
-                            var ctrl2Pos = ctrl2.UIElement.WorldMatrix.TranslationVector;
-                            float ctrl2Left = ctrl2Pos.X - (ctrl2.UIElement.RenderSize.X * 0.5f);
-                            float ctrl2Top = ctrl2Pos.Y - (ctrl2.UIElement.RenderSize.Y * 0.5f);
-                            if (ctrl1Top < ctrl2Top)
-                            {
-                                return -1;
-                            }
-                            else if (ctrl1Top > ctrl2Top)
-                            {
-                                return 1;
-                            }
-                            else
-                            {
-                                return ctrl1Left.CompareTo(ctrl2Left);
-                            }
-                        });
-                        int curSelectedIndex = allActiveControls.IndexOf(x => x.Control == _selectedControl.Control);
+                        allActiveControls.Sort(TopDownThenLeftRightSorter);
+                        int curSelectedIndex = allActiveControls.IndexOf(x => x.Control == selectedCtrl.Control);
                         int nextSelectedIndex = curSelectedIndex + (inputDir.X > 0 ? 1 : -1);
                         if (nextSelectedIndex < 0)
                         {
@@ -278,32 +228,8 @@ namespace UINavigationExample.UI
                     else
                     {
                         // Sort left to right, top to bottom
-                        allActiveControls.Sort((ctrl1, ctrl2) =>
-                        {
-                            // Use the top-left corner of the UI as the sorting position, as this should
-                            // account for different sized UI
-
-                            var ctrl1Pos = ctrl1.UIElement.WorldMatrix.TranslationVector;
-                            float ctrl1Left = ctrl1Pos.X - (ctrl1.UIElement.RenderSize.X * 0.5f);
-                            float ctrl1Top = ctrl1Pos.Y - (ctrl1.UIElement.RenderSize.Y * 0.5f);
-
-                            var ctrl2Pos = ctrl2.UIElement.WorldMatrix.TranslationVector;
-                            float ctrl2Left = ctrl2Pos.X - (ctrl2.UIElement.RenderSize.X * 0.5f);
-                            float ctrl2Top = ctrl2Pos.Y - (ctrl2.UIElement.RenderSize.Y * 0.5f);
-                            if (ctrl1Left < ctrl2Left)
-                            {
-                                return -1;
-                            }
-                            else if (ctrl1Left > ctrl2Left)
-                            {
-                                return 1;
-                            }
-                            else
-                            {
-                                return ctrl1Top.CompareTo(ctrl1Top);
-                            }
-                        });
-                        int curSelectedIndex = allActiveControls.IndexOf(x => x.Control == _selectedControl.Control);
+                        allActiveControls.Sort(LeftRightThenTopDownSorter);
+                        int curSelectedIndex = allActiveControls.IndexOf(x => x.Control == selectedCtrl.Control);
                         int nextSelectedIndex = curSelectedIndex + (inputDir.Y > 0 ? 1 : -1);
                         if (nextSelectedIndex < 0)
                         {
@@ -319,9 +245,57 @@ namespace UINavigationExample.UI
                         }
                     }
                 }
-
-                _nextControlCandidates.Clear();
             }
+        }
+
+        private static readonly Comparison<UINavControl> TopDownThenLeftRightSorter = (UINavControl ctrl1, UINavControl ctrl2) =>
+        {
+            // Use the top-left corner of the UI as the sorting position, as this should
+            // account for different sized UI
+            (float ctrl1Left, float ctrl1Top) = GetUIScreenPosition(ctrl1.UIElement);
+            (float ctrl2Left, float ctrl2Top) = GetUIScreenPosition(ctrl2.UIElement);
+            if (ctrl1Top < ctrl2Top)
+            {
+                return -1;
+            }
+            else if (ctrl1Top > ctrl2Top)
+            {
+                return 1;
+            }
+            else
+            {
+                return ctrl1Left.CompareTo(ctrl2Left);
+            }
+        };
+
+        private static readonly Comparison<UINavControl> LeftRightThenTopDownSorter = (UINavControl ctrl1, UINavControl ctrl2) =>
+        {
+            // Use the top-left corner of the UI as the sorting position, as this should
+            // account for different sized UI
+            (float ctrl1Left, float ctrl1Top) = GetUIScreenPosition(ctrl1.UIElement);
+            (float ctrl2Left, float ctrl2Top) = GetUIScreenPosition(ctrl2.UIElement);
+            if (ctrl1Left < ctrl2Left)
+            {
+                return -1;
+            }
+            else if (ctrl1Left > ctrl2Left)
+            {
+                return 1;
+            }
+            else
+            {
+                return ctrl1Top.CompareTo(ctrl1Top);
+            }
+        };
+
+        private static (float left, float top) GetUIScreenPosition(UIElement uiElement)
+        {
+            var uiPos = uiElement.WorldMatrix.TranslationVector;
+            var uiRenderSize = uiElement.RenderSize;
+            float left = uiPos.X - (uiRenderSize.X * 0.5f);
+            float top = uiPos.Y - (uiRenderSize.Y * 0.5f);
+
+            return (left, top);
         }
 
         private static IEnumerable<INavigatableControl> FindAllNavigatableControls(UIElement source)
@@ -364,12 +338,13 @@ namespace UINavigationExample.UI
         {
             _rebuildActiveControlsList = true;
 
-            if (_selectedControl.UIComponent == null)
+            if (!_selectedControl.HasValue)
             {
                 return;
             }
-            if (!_selectedControl.UIComponent.Enabled
-                || !_selectedControl.Control.IsSelectable || !_selectedControl.Control.CanNavigateToControl())
+            var selectedCtrl = _selectedControl.Value;
+            if (!selectedCtrl.UIComponent.Enabled
+                || !selectedCtrl.Control.IsSelectable || !selectedCtrl.Control.CanNavigateToControl())
             {
                 UnsetSelectedControl();
             }
@@ -379,14 +354,14 @@ namespace UINavigationExample.UI
         {
             UnsetSelectedControl();
             _selectedControl = uiNavControl;
-            _selectedControl.Control.IsSelected = true;
+            uiNavControl.Control.IsSelected = true;
         }
 
         private void UnsetSelectedControl()
         {
-            if (_selectedControl.Control != null)
+            if (_selectedControl.HasValue)
             {
-                _selectedControl.Control.IsSelected = false;
+                _selectedControl.Value.Control.IsSelected = false;
             }
             _selectedControl = default;
         }
