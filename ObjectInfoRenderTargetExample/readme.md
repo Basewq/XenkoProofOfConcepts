b8b02b771e11926a1e303f8d09006ae588fcb9a2
Basewq <Basewq@users.noreply.github.com>
2023-10-15 20:34:43 +1300

    [OIRT][SSDR] Update to Stride 4.1.0.1734. Fix breaking changes, now requires unsafe code.

--- ObjectInfoRenderTargetExample/readme.md
+++ ObjectInfoRenderTargetExample/readme.md
@@ -10,7 +10,7 @@ Example of output of RenderTarget identifying the different ModelComponents of e
 
 **Project Notes:**
 
-Ensure the .xksl and .xkfx files have the Custom Tool property set to XenkoShaderKeyGenerator in Visual Studio, so it can automatically build the .cs files. This requires the Xenko plugin for Visual Studio installed.
+Ensure the .sdsl and .sdfx files have the Custom Tool property set to XenkoShaderKeyGenerator in Visual Studio, so it can automatically build the .cs files. This requires the Xenko plugin for Visual Studio installed.
 
 **Usage Notes:**
 
@@ -18,7 +18,7 @@ To use in your own project, you will also need to set up the `GraphicsCompositor
 
 - Add an `ObjectInfo` render stage (both effect slot name and name should be the same).
 - In the `MeshRenderFeature`, add the (sub) render feature `ObjectInfoRenderFeature`, and the render stage selector `ObjectInfoRenderStageSelector`.
-- For `ObjectInfoRenderStageSelector`, set the Effect Name as `ObjectInfoOutputEffect` (this is the name of the effect in `Effects\ObjectInfoOutputEffect.xkfx`), set the Object Info Render Stage to the above `ObjectInfo`, Render Group can be set to whatever objects you want to be written to the texture.
+- For `ObjectInfoRenderStageSelector`, set the Effect Name as `ObjectInfoOutputEffect` (this is the name of the effect in `Effects\ObjectInfoOutputEffect.sdfx`), set the Object Info Render Stage to the above `ObjectInfo`, Render Group can be set to whatever objects you want to be written to the texture.
 - If running with the default setup, select the `Entry points` node, add `ObjectInfoSceneRenderer` as a child in the property (Game renderer -> Camera Renderer -> Child -> SceneRendererCollection -> Children).
 
   **Important Note:** Make this appear *before* the `Shared: Forward renderer` renderer (easiest way is to add to the end, save, then modifier the `Assets\GraphicsCompositor.xkgfxcomp` file directly to shift it.)
@@ -31,13 +31,13 @@ The example scene has the "Wall" entity in Render group `Group10`, and the `Obje
 ### Code Explanation
 
 `ObjectInfoSceneRenderer` does the actual encoding (rendering) of the object info. Since it is expected to be used by the standard rendering system (eg. used in a custom shader on a material), you must make sure this renderer is ordered *before* the Forward renderer.
-At the end of the draw call, it will set up the generated texture for consumption (consumption example is the shader `ObjectInfoOutputTestShader.xksl`).
+At the end of the draw call, it will set up the generated texture for consumption (consumption example is the shader `ObjectInfoOutputTestShader.sdsl`).
 
-The `ObjectInfoRenderFeature` generates the "buffer" data, ie. the entity data we want the `ObjectInfoOutputEffect` to read and output, and `ObjectInfoRenderStageSelector` declares the effect/shader that reads this buffer data (`ObjectInfoOutputEffect.xkfx` & `ObjectInfoOutputShader.xksl`, where the Effect Name is declared in the Graphics Compositor).
+The `ObjectInfoRenderFeature` generates the "buffer" data, ie. the entity data we want the `ObjectInfoOutputEffect` to read and output, and `ObjectInfoRenderStageSelector` declares the effect/shader that reads this buffer data (`ObjectInfoOutputEffect.sdfx` & `ObjectInfoOutputShader.sdsl`, where the Effect Name is declared in the Graphics Compositor).
 
 Note that in order for skinned (animated) models to be rendered correctly on the texture, we need to inherit the shaders that compute the skinned transformations. Unfortunately, we cannot add the mixin `XenkoForwardShadingEffect`, due to heavy ties with `ShaderBase`. The problem with `ShaderBase` is that it declares the pixel output (`SV_Target0`) as `float4`, which works fine for a `float4` format texture, but our object info texture is `uint2` format, so we want a pixel output of `uint2`.
 
-To solve this, we essentially copy `XenkoForwardShadingEffect.xkfx` and most of the mixin shaders that it uses, and change every shader that inherits `ShaderBase` to inherit our `OioShaderBase` instead, which declares `SV_Target0` as `uint2`. All these shaders are placed in `Effects\ObjectInfoXenkoBaseShaders` folder.
+To solve this, we essentially copy `XenkoForwardShadingEffect.sdfx` and most of the mixin shaders that it uses, and change every shader that inherits `ShaderBase` to inherit our `OioShaderBase` instead, which declares `SV_Target0` as `uint2`. All these shaders are placed in `Effects\ObjectInfoXenkoBaseShaders` folder.
 
 ---
 ### Graphics Compositor Setup
--- ScreenSpaceDecalExample/ScreenSpaceDecalExample/ObjectInfoRenderer/ObjectInfoRenderFeature.cs
+++ ScreenSpaceDecalExample/ScreenSpaceDecalExample/ObjectInfoRenderer/ObjectInfoRenderFeature.cs
@@ -3,6 +3,7 @@ using Stride.Core.Mathematics;
 using Stride.Core.Threading;
 using Stride.Engine;
 using Stride.Rendering;
+using System.Runtime.CompilerServices;
 
 namespace ScreenSpaceDecalExample.ObjectInfoRenderer
 {
@@ -10,7 +11,7 @@ namespace ScreenSpaceDecalExample.ObjectInfoRenderer
     {
         // Make sure it's public so it is visible to Game Studio.
         // In Game Studio, open the Graphics Compositor asset -> Render features -> MeshRenderFeature
-        // Add 'ObjectInfoRenderFeature ' to Render Features in the property grid of the MeshRenderFeature.
+        // Add 'ObjectInfoRenderFeature' to Render Features in the property grid of the MeshRenderFeature.
 
         private ObjectPropertyKey<ObjectInfoData> _objectInfoPropertyKey;
         private ConstantBufferOffsetReference _objectInfoDataBuffer;
@@ -43,14 +44,14 @@ namespace ScreenSpaceDecalExample.ObjectInfoRenderer
             foreach (var objectNodeReference in RootRenderFeature.ObjectNodeReferences)
             {
                 var objectNode = RootRenderFeature.GetObjectNode(objectNodeReference);
-                if (!(objectNode.RenderObject is RenderMesh renderMesh))
+                if (objectNode.RenderObject is not RenderMesh renderMesh)
                 {
                     continue;
                 }
 
                 blendMatricesDataHolder[objectNodeReference] = renderMesh.BlendMatrices;    // This is for our skinned models.
 
-                if (!(renderMesh.Source is ModelComponent modelComponent))
+                if (renderMesh.Source is not ModelComponent modelComponent)
                 {
                     continue;
                 }
@@ -74,14 +75,12 @@ namespace ScreenSpaceDecalExample.ObjectInfoRenderer
         public override void Prepare(RenderDrawContext context)
         {
             // This entire method shows how we pass the ObjectInfoData to the shader, and is similar to
-            // how Stride does it when it needs to pass data. The only change is we use Utilities.Write
-            // where the underlying method call handles the pointer writing code, so we can avoid
-            // marking this project with 'unsafe' code.
+            // how Stride does it when it needs to pass data.
             var objectInfoDataHolder = RootRenderFeature.RenderData.GetData(_objectInfoPropertyKey);
             Dispatcher.ForEach(((RootEffectRenderFeature)RootRenderFeature).RenderNodes, (ref RenderNode renderNode) =>
             {
                 // PerDrawLayout means we access this data in the shader via
-                // cbuffer PerDraw { ... } (see ObjectInfoOutputShader.xlsl)
+                // cbuffer PerDraw { ... } (see ObjectInfoOutputShader.sdsl)
                 var perDrawLayout = renderNode.RenderEffect.Reflection?.PerDrawLayout;
                 if (perDrawLayout == null)
                 {
@@ -94,9 +93,14 @@ namespace ScreenSpaceDecalExample.ObjectInfoRenderer
                     return;
                 }
 
-                var objectInfoData = objectInfoDataHolder[renderNode.RenderObject.ObjectNode];
-                var mappedCBPtr = renderNode.Resources.ConstantBuffer.Data;
-                Utilities.Write(mappedCBPtr + objectInfoDataOffset, ref objectInfoData);
+                // This is similar to TransformRenderFeature.Prepare
+                unsafe
+                {
+                    ref var srcObjectInfoData = ref objectInfoDataHolder[renderNode.RenderObject.ObjectNode];
+                    var mappedConstBufferIntPtr = renderNode.Resources.ConstantBuffer.Data;
+                    var destPtr = (ObjectInfoData*)((byte*)mappedConstBufferIntPtr + objectInfoDataOffset);
+                    *destPtr = srcObjectInfoData;
+                }
             });
 
             // We can probably combine this with the above ForEach method, but separated here
@@ -122,8 +126,17 @@ namespace ScreenSpaceDecalExample.ObjectInfoRenderer
                     return;
                 }
 
-                var mappedCBPtr = renderNode.Resources.ConstantBuffer.Data;
-                Utilities.Write(mappedCBPtr + blendMatricesOffset, renderModelObjectInfo, 0, renderModelObjectInfo.Length);
+                // This is similar to SkinningRenderFeature.Prepare
+                unsafe
+                {
+                    var mappedConstBufferIntPtr = renderNode.Resources.ConstantBuffer.Data;
+                    uint arraySizeInBytes = (uint)(Matrix.SizeInBytes * renderModelObjectInfo.Length);
+                    var destPtr = (byte*)mappedConstBufferIntPtr + blendMatricesOffset;
+                    fixed (Matrix* srcObjectInfoPtr = renderModelObjectInfo)
+                    {
+                        Unsafe.CopyBlockUnaligned(destPtr, srcObjectInfoPtr, arraySizeInBytes);
+                    }
+                }
             });
         }
     }
